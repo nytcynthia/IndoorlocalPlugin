6028171aca21b340e1d90850ebe1b8cad8243efb
nytcynthia <nytcynthia@gmail.com>
2015-04-20 09:48:37 +0800

    add indoorLocationManager.stopLocation();

--- src/android/WheramiPlugin.java
+++ src/android/WheramiPlugin.java
@@ -91,11 +91,13 @@ public class WheramiPlugin extends CordovaPlugin {
                                 .append(point.y).append(",").append(radius[i]).append("\n");
                         i++;
                     }
+                    indoorLocationManager.stopLocation();
                     callbackContext.success(positionFoundString.toString());
                 } else {
+                    indoorLocationManager.stopLocation();
                     callbackContext.error("Calculate failed, waiting another try");
                 }
-                indoorLocationManager.stopLocation();
+
             }
         });
     }
